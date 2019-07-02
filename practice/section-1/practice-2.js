a4aa3f5a4840c3b95eca13f3c922b4ca61226e29
cyang <shmilycherish@gmail.com>
2019-07-02 21:12:22 +0800

    implement practice 2

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,5 +1,6 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  let flatCollection = collectionB.reduce((pre, arr) => pre.concat(arr), []);
+  return collectionA.filter(element => flatCollection.includes(element));
 }
